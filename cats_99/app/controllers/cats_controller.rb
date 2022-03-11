f10615baf0a778ce29e482c9c7fb8fadf6693133
App Academy Student <email@email.com>
2022-03-11 15:45:36 -0500

    break

--- cats_99/app/controllers/cats_controller.rb
+++ cats_99/app/controllers/cats_controller.rb
@@ -17,6 +17,32 @@ class CatsController < ApplicationController
     render :new
   end
 
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else 
+      render json: @cat.errors.full_messages, status: 422
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+
+    if @cat.update(cat_params)
+      redirect_to cats_url(@cat)
+    else  
+      render json: @cat.errors.full_messages, status: 422
+    end
+  end
+
   def cat_params
     params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
   end
